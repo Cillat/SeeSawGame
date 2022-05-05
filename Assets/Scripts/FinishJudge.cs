064764f07197467de4149dba0f44f3c41833fa42
Basshi <Kurorie96@outlook.jp>
2022-05-05 18:26:43 +0900

    UPDATE:微細な修正

--- Assets/Scripts/FinishJudge.cs
+++ Assets/Scripts/FinishJudge.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FinishJudge : MonoBehaviour
 {
-    GameManager gameManager;
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
