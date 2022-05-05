84529e2823f87bd30a8e073d3fc3929fde9c47a6
Basshi <Kurorie96@outlook.jp>
2022-05-06 01:22:37 +0900

    ADD:リセットフラグ作成
    InstatntiateされたPlayer Animalsにつく．
    Playerスクリプトのフラグをリセットする

---
+++ Assets/Scripts/ResetFlag.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetFlag : MonoBehaviour
+{
+
+    private Player mPlayer;
+    private int mCollisionFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPlayer = GameObject.Find("GameStage/GameManager").GetComponent<Player>();
+        mCollisionFlag = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (mPlayer.selectFlag == 2)
+        {
+            if (mCollisionFlag == 0)
+            {
+                mCollisionFlag = 1;
+                mPlayer.selectFlag = 0;
+                mPlayer.spawnFlag = 0;
+
+                if (mPlayer.selectAnimals[0] != null)
+                {
+                    Destroy(mPlayer.selectAnimals[0]);
+                }
+
+                if (mPlayer.selectAnimals[1] != null)
+                {
+                    Destroy(mPlayer.selectAnimals[1]);
+                }
+
+                if (mPlayer.selectAnimals[2] != null)
+                {
+                    Destroy(mPlayer.selectAnimals[2]);
+                }
+            }
+        }
+    }
+}
