2217a5783f64cf8fa3f065e014c3b8c883cae01e
Basshi <Kurorie96@outlook.jp>
2022-05-06 18:44:03 +0900

    UPDATE:大幅修正

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,64 +6,79 @@ public class Player : MonoBehaviour
 {
 
     private GameManager gameManager;
-    private GameObject[] mPlayerGameObjects;
     private GameObject mPlayerChoice;
     private Camera mCamera;
+    private Animal mAnimal;
     private int[] mAnimalNum;
-    private float mTime;
     private float mMouseWheelScroll;
     private float mMouseDepth;
     private Vector3 mMousePos;
+
     public int spawnFlag;
     public int selectFlag;
+    public Dictionary<int, Dictionary<GameObject,float>> dicPlayerGameObjects;
     public GameObject[] selectAnimals;
+    public GameObject[] mDicAnimalKeys;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameStage/GameManager").GetComponent<GameManager>();
+        mAnimal = GameObject.Find("GameStage/GameManager").GetComponent<Animal>();
         mCamera = GameObject.Find("GameStage/PlayerView/PlayerCamera").GetComponent<Camera>();
         mPlayerChoice = null;
         mAnimalNum = new int[3];
         selectAnimals = new GameObject[3];
-        mPlayerGameObjects = new GameObject[13];
-        mTime = 0f;
         mMouseDepth = 16.5f;
         spawnFlag = 0;
         selectFlag = 0;
 
-        mPlayerGameObjects[0] = GameObject.Find("PlayerSelectAnimals/PlayerRat");
-        mPlayerGameObjects[1] = GameObject.Find("PlayerSelectAnimals/PlayerRabbit");
-        mPlayerGameObjects[2] = GameObject.Find("PlayerSelectAnimals/PlayerRooster");
-        mPlayerGameObjects[3] = GameObject.Find("PlayerSelectAnimals/PlayerCrane");
-        mPlayerGameObjects[4] = GameObject.Find("PlayerSelectAnimals/PlayerSnake");
-        mPlayerGameObjects[5] = GameObject.Find("PlayerSelectAnimals/PlayerDog");
-        mPlayerGameObjects[6] = GameObject.Find("PlayerSelectAnimals/PlayerMonkey");
-        mPlayerGameObjects[7] = GameObject.Find("PlayerSelectAnimals/PlayerGoat");
-        mPlayerGameObjects[8] = GameObject.Find("PlayerSelectAnimals/PlayerPig");
-        mPlayerGameObjects[9] = GameObject.Find("PlayerSelectAnimals/PlayerOx");
-        mPlayerGameObjects[10] = GameObject.Find("PlayerSelectAnimals/PlayerHorse");
-        mPlayerGameObjects[11] = GameObject.Find("PlayerSelectAnimals/PlayerTiger");
-        mPlayerGameObjects[12] = GameObject.Find("PlayerSelectAnimals/PlayerDragon");
+
+        //dicPlayerGameObjects = new Dictionary<int, Dictionary<GameObject, float>>()
+        //{
+            //{ 0, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerRat"), 1f } } },
+            //{ 1, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerRabbit"), 2f } } },
+            //{ 2, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerRooster"), 3f } } },
+            //{ 3, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerCrane"), 4f } } },
+            //{ 4, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerSnake"), 5f } } },
+            //{ 5, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerDog"), 6f } } },
+            //{ 6, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerMonkey"), 7f } } },
+            //{ 7, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerGoat"), 8f } } },
+            //{ 8, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerPig"), 9f } } },
+            //{ 9, new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerOx"), 10f } } },
+            //{ 10,new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerHorse"), 11f } } },
+            //{ 11,new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerTiger"), 12f } } },
+            //{ 12,new Dictionary<GameObject, float>{ { GameObject.Find("PlayerSelectAnimals/PlayerDragon"), 13f } } },
+
+        //};
     }
+
+        //mDicAnimalKeys = new GameObject[dicPlayerGameObjects.Count];
+
+        //foreach (var kv in dicPlayerGameObjects)
+        //{
+         //   dicPlayerGameObjects.Keys.CopyTo(mDicAnimalKeys, kv.Value);
+        //}
 
     // Update is called once per frame
     void Update()
     {
+
         if (spawnFlag == 0)
         {
             mAnimalNum[0] = Random.Range(0, 13);
             mAnimalNum[1] = Random.Range(0, 13);
             mAnimalNum[2] = Random.Range(0, 13);
-            spawnFlag = 1;
 
-            selectAnimals[0] = Instantiate(mPlayerGameObjects[mAnimalNum[0]], new Vector3(-97f, 0.7f, 5.3f) , Quaternion.identity);
-            selectAnimals[1] = Instantiate(mPlayerGameObjects[mAnimalNum[1]], new Vector3(-100f, 0.7f, 5.3f), Quaternion.identity);
-            selectAnimals[2] = Instantiate(mPlayerGameObjects[mAnimalNum[2]], new Vector3(-103f, 0.7f, 5.3f), Quaternion.identity);
+            selectAnimals[0] = Instantiate(mAnimal.playerAnimals[mAnimalNum[0]].playerAnimalName, new Vector3(-97f, 0.7f, 5.3f) , Quaternion.identity);
+            selectAnimals[1] = Instantiate(mAnimal.playerAnimals[mAnimalNum[1]].playerAnimalName, new Vector3(-100f, 0.7f, 5.3f), Quaternion.identity);
+            selectAnimals[2] = Instantiate(mAnimal.playerAnimals[mAnimalNum[2]].playerAnimalName, new Vector3(-103f, 0.7f, 5.3f), Quaternion.identity);
+
+            spawnFlag = 1;
         }
 
         mMousePos = Input.mousePosition;
-        Debug.Log(mMousePos);
+        //Debug.Log(mMousePos);
         mMouseWheelScroll = Input.GetAxis("Mouse ScrollWheel");
 
         if (mMouseWheelScroll > 0f)
@@ -94,7 +109,7 @@ public class Player : MonoBehaviour
             touchWorldPosition.x = -96f;
         }
 
-        Debug.Log(touchWorldPosition);
+        //Debug.Log(touchWorldPosition);
 
         if (selectFlag == 0)
         {
@@ -106,12 +121,12 @@ public class Player : MonoBehaviour
 
                 if (Physics.Raycast(ray, out hit))
                 {
-                    Debug.Log(hit.collider.gameObject.transform.position);
-                    Debug.Log(hit.collider.gameObject.tag);
+                    //Debug.Log(hit.collider.gameObject.transform.position);
+                    //Debug.Log(hit.collider.gameObject.tag);
 
                     if(hit.collider.gameObject.CompareTag("PlayerSelectAnimals"))
                     {
-                        Debug.Log(hit.collider.gameObject);
+                        //Debug.Log(hit.collider.gameObject);
 
                         selectFlag = 1;
                         mPlayerChoice = Instantiate(hit.collider.gameObject, new Vector3(touchWorldPosition.x, 13f, touchWorldPosition.z), Quaternion.identity);
@@ -119,6 +134,7 @@ public class Player : MonoBehaviour
                         mPlayerChoice.AddComponent<Rigidbody>();
                         mPlayerChoice.AddComponent<ResetFlag>();
                         mPlayerChoice.AddComponent<DestroyAnimal>();
+                        mPlayerChoice.AddComponent<PlayerAnimalWeight>();
 
                         Destroy(hit.collider.gameObject);
                     }
@@ -134,14 +150,5 @@ public class Player : MonoBehaviour
                 selectFlag = 2;
             }
         }
-
-
-//        mTime += Time.deltaTime;
-//        if (mTime > 5f)
-//        {
-//           mTime = 0f;
-//            spawnFlag = 0;
-//        }
-
     }
 }
