28f1e6af7a3d8719389d36986f5507a72777e7dd
Basshi <Kurorie96@outlook.jp>
2022-05-06 18:44:28 +0900

    UPDATE:オブジェクトの名前からその重さを取得する．
    上手く行っていない

--- Assets/Scripts/PlayerAnimalWeight.cs
+++ Assets/Scripts/PlayerAnimalWeight.cs
@@ -1,32 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerAnimalWeight : MonoBehaviour
 {
     private GameObject mAnimalName;
-    private Player mPlayer;
+    private Animal mAnimal;
+    private Animal.PlayerAnimals mPlayerAnimals;
     private GameManager mGamemanager;
-    private float mAnimalWeight;
+     
+    
+    private float weight;
 
     // Start is called before the first frame update
     void Start()
     {
         mAnimalName = this.gameObject;
-        mPlayer = GameObject.Find("GameStage/GameManager").GetComponent<Player>();
+        
         mGamemanager = GameObject.Find("GameStage/GameManager").GetComponent<GameManager>();
-        mAnimalWeight = mPlayer.dicPlayerGameObjects[mAnimalName];
+        mAnimal = GameObject.Find("GameStage/GameManager").GetComponent<Animal>();
+        mPlayerAnimals = mAnimal.playerAnimals.Find(animal => mAnimalName == animal.playerAnimalName);
 
-        mGamemanager.playerWeight += mAnimalWeight;
+        mGamemanager.playerWeight += mPlayerAnimals.playerAnimalWeight;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(mPlayerAnimals.playerAnimalWeight);
+        //Debug.Log(mPlayerAnimals.playerAnimalWeight);
         if (this.gameObject.GetComponent<DestroyAnimal>().destroyFlag == 1)
         {
-            mGamemanager.playerWeight -= mAnimalWeight; 
+            mGamemanager.playerWeight -= mPlayerAnimals.playerAnimalWeight; 
         }
     }
 }
