f31441644da6b5db6457eebd274fabb28cd67238
Basshi <Kurorie96@outlook.jp>
2022-05-08 18:33:03 +0900

    UPDATE:ゲーム終了になったら終了の音を鳴らしBGMを止めるようにした

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,34 +17,48 @@ public class GameManager : MonoBehaviour
     public SceneFlag sceneFlag;
     public float playerWeight;
     public float enemyWeight;
-    static float mPlayTime = 0f;
+
+    private AudioManager mAudioManager;
     private GameObject mSeeSaw;
+    private GameObject mPlayerBox;
+    private GameObject mEnemyBox;
+    private static  float mPlayTime = 0f;
     private float mDifferenceWeight;
     private float mLeanGain;
     private float finishTime;
     private float mTime;
 
+    private int mFinishSound;
+
     // Start is called before the first frame update
     void Start()
     {
+        AudioManager.Instance.PlayBGM("Main");
+
         sceneFlag = SceneFlag.Playing;
         playerWeight = 0f;
         enemyWeight = 0f;
+        mAudioManager = this.gameObject.GetComponent<AudioManager>();
         mSeeSaw = GameObject.Find("GameStage/SeeSawView/WholeSeeSaw/SeeSaw");
+        mPlayerBox = GameObject.Find("GameStage/PlayerView/PlayerBoxes");
+        mEnemyBox = GameObject.Find("GameStage/EnemyView/EnemyBoxes");
         mTime = 0;
         mDifferenceWeight = 0f;
         mLeanGain = 0f;
 
+        mFinishSound = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         mTime += Time.deltaTime;
-        mDifferenceWeight = (playerWeight - enemyWeight) * 1000;
-        mLeanGain = mDifferenceWeight / 10000000f;
+        mDifferenceWeight = (playerWeight - enemyWeight) * 100;
+        mLeanGain = mDifferenceWeight / 150000f;
 
         mSeeSaw.transform.Rotate(new Vector3(0f, 0f, mLeanGain));
+        mPlayerBox.transform.Rotate(new Vector3(-1 * mLeanGain, 0f, 0f));
+        mEnemyBox.transform.Rotate(new Vector3(0f, 0f, mLeanGain));
 
 
         if (mTime > 5f)
@@ -65,7 +79,15 @@ public class GameManager : MonoBehaviour
         else if(sceneFlag == SceneFlag.Finish)
         {
             finishTime = Timer;
-            Debug.Log("Finish!!");
+
+            if(mFinishSound == 0)
+            {
+                mFinishSound = 1;
+                Debug.Log("Finish!!");
+                mAudioManager.FadeOutBGM();
+                AudioManager.Instance.PlaySE("Finish");
+
+            }
         }
 
         
