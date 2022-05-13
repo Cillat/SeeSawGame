053c0624a97a23ab29f8082cbcec325c42256b6f
Basshi <Kurorie96@outlook.jp>
2022-05-13 19:21:14 +0900

    UPDATE:ランキング送信
    まだ未完成

--- Assets/Scripts/ResultRanking.cs
+++ Assets/Scripts/ResultRanking.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class ResultRanking : MonoBehaviour
 {
     private GameManager mGameManager;
-    // Start is called before the first frame update
+    private int mMinute;
+    private int mSecond;
+    private int mMillSecond;
+
     void Start()
     {
         mGameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        //var millsec = mGameManager.Timer;
-        //var timeScore = new System.TimeSpan(0, 0, 0, 0, millsec);
-        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(1555555555555);
+        mMinute = (int)mGameManager.Timer / 60;
+        mSecond = (int)mGameManager.Timer - mMinute * 60;
+        mMillSecond = (int)(mGameManager.Timer - Mathf.Floor(mGameManager.Timer)) * 100;
 
+        var timeScore = new System.TimeSpan(0, 0, mMinute, mSecond, mMillSecond);
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(timeScore);
     }
 
     // Update is called once per frame
