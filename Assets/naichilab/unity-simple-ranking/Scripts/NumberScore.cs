42194c1a8a652fb8d43db722bef907a87717cc28
Basshi <Kurorie96@outlook.jp>
2022-05-13 16:51:56 +0900

    ADD:パッケージインストール

---
+++ Assets/naichilab/unity-simple-ranking/Scripts/NumberScore.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace naichilab
+{
+    /// <summary>
+    /// 数値型スコア
+    /// </summary>
+    public class NumberScore : IScore
+    {
+        //double →　floatに変更
+        private double score;
+        private string format;
+
+
+        public NumberScore(double score, string format = "")
+        {
+            this.score = score;
+            this.format = format;
+        }
+
+        public ScoreType Type
+        {
+            get { return ScoreType.Number; }
+        }
+
+        public string TextForDisplay
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(format))
+                {
+                    return score.ToString(format);
+                }
+                else
+                {   //変更点
+                    return score.ToString("F2");
+                    //F2を追加
+                }
+            }
+        }
+
+        public string TextForSave
+        {
+            get { return score.ToString(); }
+        }
+
+        public double Value
+        {
+            get { return score; }
+        }
+    }
+}
